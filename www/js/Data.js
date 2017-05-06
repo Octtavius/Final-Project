c938e6e664ff1894bbbe2917c2f9fd4474bf8d01
Morcov <octavian.morcov@sap.com>
2017-05-06 13:41:07 +0100

    dsjflj

---
+++ www/js/Data.js
@@ -0,0 +1,298 @@
+(function () {
+  var Data = function () {
+    var cars = [
+      {
+        car_id: "111",
+        imgUrl: "img/mercedes1.png",
+        brand: {
+          title: "Mercedes-Benz",
+          icon: "ion-model-s"
+        },
+        model: {
+          title: "B-Class",
+          icon: "ion-pricetag"
+        },
+        engine: {
+          title: "1.6 Diesel "
+        },
+        price: {
+          title: 26877,
+          icon: "ion-social-euro"
+        },
+        year: {
+          title: "2017",
+          icon: "ion-calendar"
+        },
+        transmission: {
+          title: "Manual"
+        },
+        // eco: {
+        //   title: "Eco",
+        //   icon: "ion-leaf"
+        // },
+        maxSpeed: {
+          title: "200",
+          icon: "ion-ios-speedometer"
+        },
+        consumes: {
+          title: "7.6/7.4",
+          icon: "ion-waterdrop"
+        },
+        wheelSystem: {
+          title: "4x2"
+        },
+        weight: {
+          title: 1960
+        }
+      },
+      {
+        car_id: "112",
+        imgUrl: "img/Mercedes-AMG-GT-C-Roadster.png",
+        brand: {
+          title: "Mercedes-Benz",
+          icon: "ion-model-s"
+        },
+        model: {
+          title: "AMG GT C Roadster",
+          icon: "ion-pricetag"
+        },
+        engine: {
+          title: "3.9 Petrol Twin",
+          icon: "ion-"
+        },
+        price: {
+          title: 90000,
+          icon: "ion-social-euro"
+        },
+        year: {
+          title: "2017",
+          icon: "ion-calendar"
+        },
+        transmission: {
+          title: "Manual",
+          icon: "ion-"
+        },
+        // eco: {
+        //   title: "Eco",
+        //   icon: "ion-leaf"
+        // },
+        maxSpeed: {
+          title: "316",
+          icon: "ion-ios-speedometer"
+        },
+        consumes: {
+          title: "9.5",
+          icon: "ion-waterdrop"
+        },
+        wheelSystem: {
+          title: "4x2 Rear-Wheel",
+          icon: "ion-"
+        },
+        weight: {
+          title: 2000
+        }
+      },
+      {
+        car_id: "113",
+        imgUrl: "img/vw_scirocco.png",
+        brand: {
+          title: "Volkswagen",
+          icon: "ion-model-s"
+        },
+        model: {
+          title: "Scirocco",
+          icon: "ion-pricetag"
+        },
+        engine: {
+          title: "2.0 Diesel ",
+          icon: "ion-"
+        },
+        price: {
+          title: 27500,
+          icon: "ion-social-euro"
+        },
+        year: {
+          title: "2017",
+          icon: "ion-calendar"
+        },
+        transmission: {
+          title: "Manual"
+        },
+        // eco: {
+        //   title: "Eco",
+        //   icon: "ion-leaf"
+        // },
+        maxSpeed: {
+          title: "230",
+          icon: "ion-ios-speedometer"
+        },
+        consumes: {
+          title: "5.4",
+          icon: "ion-waterdrop"
+        },
+        wheelSystem: {
+          title: "4x2 Front-Wheels"
+        },
+        weight: {
+          title: 1800
+        }
+      },
+      {
+        car_id: "114",
+        imgUrl: "img/volkswagen_beetle.png",
+        brand: {
+          title: "Volkswagen",
+          icon: "ion-model-s"
+        },
+        model: {
+          title: "Beetle",
+          icon: "ion-pricetag"
+        },
+        engine: {
+          title: "2.0 Petrol(Gasoline) ",
+          icon: "ion-"
+        },
+        price: {
+          title: 20000,
+          icon: "ion-social-euro"
+        },
+        year: {
+          title: "2016",
+          icon: "ion-calendar"
+        },
+        transmission: {
+          title: "Manual",
+          icon: "ion-"
+        },
+        // eco: {
+        //   title: "Eco",
+        //   icon: "ion-leaf"
+        // },
+        maxSpeed: {
+          title: "185",
+          icon: "ion-ios-speedometer"
+        },
+        consumes: {
+          title: "8.7",
+          icon: "ion-waterdrop"
+        },
+        wheelSystem: {
+          title: "4x2 Front-Wheel",
+          icon: "ion-"
+        },
+        weight: {
+          title: 16500
+        }
+      },
+      {
+        car_id: "115",
+        imgUrl: "img/bmw-i3.png",
+        brand: {
+          title: "BMW",
+          icon: "ion-model-s"
+        },
+        model: {
+          title: "i3",
+          icon: "ion-pricetag"
+        },
+        engine: {
+          title: "Electric",
+          icon: "ion-"
+        },
+        price: {
+          title: 35000,
+          icon: "ion-social-euro"
+        },
+        year: {
+          title: "2017",
+          icon: "ion-calendar"
+        },
+        transmission: {
+          title: "Manual Single Speed"
+        },
+        // eco: {
+        //   title: "Eco",
+        //   icon: "ion-leaf"
+        // },
+        maxSpeed: {
+          title: "160",
+          icon: "ion-ios-speedometer"
+        },
+        consumes: {
+          title: "0.6",
+          icon: "ion-waterdrop"
+        },
+        wheelSystem: {
+          title: "4x2 Rear-Wheels"
+        },
+        weight: {
+          title: 1300
+        }
+      },
+      {
+        car_id: "116",
+        imgUrl: "img/A7.png",
+        brand: {
+          title: "Audi",
+          icon: "ion-model-s"
+        },
+        model: {
+          title: "A7",
+          icon: "ion-pricetag"
+        },
+        engine: {
+          title: "3.0 Diesel",
+        },
+        price: {
+          title: 63000,
+          icon: "ion-social-euro"
+        },
+        year: {
+          title: "2017",
+          icon: "ion-calendar"
+        },
+        transmission: {
+          title: "Manual 7 Speed",
+          icon: "ion-"
+        },
+        maxSpeed: {
+          title: "240",
+          icon: "ion-ios-speedometer"
+        },
+        consumes: {
+          title: "4.7",
+          icon: "ion-waterdrop"
+        },
+        wheelSystem: {
+          title: "4x2 Front-Drive",
+          icon: "ion-"
+        },
+        weight: {
+          title: 2300
+        }
+      }
+    ];
+
+    var byId = function (id) {
+      console.log(id);
+      for(var i=0;i<cars.length;i++){
+        if(cars[i].car_id===id){
+          return cars[i];
+        }
+      }
+    }
+
+    var getCars = function () {
+      return cars;
+    };
+
+    return {
+      getAllCars: getCars,
+      carById: byId
+    }
+  };
+
+  var module = angular.module('starter');
+
+  module.factory("Data", Data);
+}());
---
+++ www/js/app.js
@@ -0,0 +1,165 @@
+angular.module('starter', ['ionic'])
+
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('tabs', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+      })
+
+
+      .state('tabs.first', {
+        url: "/first",
+        views: {
+          'home-tab': {
+            templateUrl: "templates/first.html"
+          }
+        }
+      })
+
+
+      .state('tabs.home', {
+        url: "/home/:id",
+        views: {
+          'home-tab': {
+            templateUrl: "templates/home.html",
+            controller: 'firstCtrl'
+          }
+        }
+      })
+      .state('tabs.facts', {
+        url: "/facts",
+        views: {
+          'home-tab': {
+            templateUrl: "templates/facts.html",
+            controller: 'AssistanceCtrl'
+          }
+        }
+      })
+      .state('tabs.facts2', {
+        url: "/facts2",
+        views: {
+          'home-tab': {
+            templateUrl: "templates/facts2.html"
+          }
+        }
+      })
+      .state('tabs.about', {
+        url: "/about",
+        views: {
+          'about-tab': {
+            templateUrl: "templates/about.html",
+            controller: "AllCarsCtrl"
+          }
+        }
+      })
+      .state('tabs.navstack', {
+        url: "/navstack",
+        views: {
+          'about-tab': {
+            templateUrl: "templates/nav-stack.html"
+          }
+        }
+      })
+      .state('tabs.contact', {
+        url: "/contact",
+        views: {
+          'contact-tab': {
+            templateUrl: "templates/contact.html",
+            controller: "GalleryCtrl"
+          }
+        }
+      });
+    $urlRouterProvider.otherwise("/tab/home/112");
+  })
+  .controller("GalleryCtrl", function ($scope, cam) {
+    $scope.title = "My Photos";
+    $scope.allpic = [];
+
+    //when screen page is loaded, display somehting to console
+    // $scope.$on('$ionicView.afterEnter', function(){
+    console.log("After Entered Interactive Guide");
+    $scope.allpic = cam.getAllFotos;
+    // $scope.allpic = ["A7", "mercedes1", "tesla_model_s", "volkswagen_beetle", "wheelSystem", "temp"];
+    // $scope.allpic = ["DSC_0062", "DSC_0063", "DSC_0064", "DSC_0065", "DSC_0066"];
+    // console.log($scope.allpic.length);
+    $scope.things = StorageService.getAll();
+    console.log($scope.things.length);
+  })
+  .controller("AssistanceCtrl", function ($scope) {
+    $scope.title111 = "Assistance page"
+  })
+  .controller("AllCarsCtrl", function ($scope, Data) {
+    $scope.title = "Photo Gallery";
+    $scope.allCars = Data.getAllCars()
+  })
+  .controller('firstCtrl', function($scope, Data, $state) {
+    var carId = $state.params.id;
+    $scope.car = Data.carById(carId)
+    // $scope.toggleRight = function () {
+    //   console.log("hhhhhh");
+    //   $ionicSideMenuDelegate.toggleRight();
+    // }
+    $scope.title = "Interactive Beacon App";
+
+    $scope.takePhoto = function () {
+      console.log("take photo");
+      // cam.takePhoto()
+    }
+  })
+
+
+  .directive('showTabs', function(ngIfDirective) {
+    var ngIf = ngIfDirective[0];
+
+    return {
+      transclude: ngIf.transclude,
+      priority: ngIf.priority,
+      terminal: ngIf.terminal,
+      restrict: ngIf.restrict,
+      link: function($scope, $element, $attr) {
+        var value = $attr['showTabs'];
+        var yourCustomValue = $scope.$eval(value);
+        var tabs = document.querySelector(".tabs");
+        console.log(tabs);
+        console.log(yourCustomValue);
+
+        $attr.ngIf = function() {
+          return yourCustomValue;
+        };
+
+        ngIf.link.apply(ngIf, arguments);
+      }
+    };
+  })
+.factory("cam", function ($cordovaCamera, StorageService) {
+  var allPic = [];
+  var tp = function() {
+    var options = {
+      quality : 90,
+      destinationType : Camera.DestinationType.DATA_URL,
+      sourceType : Camera.PictureSourceType.CAMERA,
+      allowEdit : false,
+      encodingType: Camera.EncodingType.JPEG,
+      correctOrientation: true,
+      // targetWidth: 300,
+      // targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: true
+    };
+
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+      allPic.push("data:image/jpeg;base64," + imageData);
+      StorageService.add("data:image/jpeg;base64," + imageData);
+    }, function(err) {
+      // An error occured. Show a message to the user
+    });
+  }
+
+  return{
+    takePhoto:tp,
+    getAllFotos:allPic
+  }
+})
